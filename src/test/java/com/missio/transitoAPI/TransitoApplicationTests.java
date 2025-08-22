4871bfe1a8a4bc7d9be765187d6d96da96e66aeb
RafaelMissio <rtmissio@gmail.com>
2025-08-22 09:57:05 -0300

    first commit

---
+++ src/test/java/com/missio/transitoAPI/TransitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.missio.transitoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
